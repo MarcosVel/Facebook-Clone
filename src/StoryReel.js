32e31f7cf98ad3677e52c58c6962f56f49d04669
MarcosVel <marcos.veloso@sempreceub.com>
2020-10-14 00:18:22 -0300

    Adicionando favicon e melhorias no css

--- src/StoryReel.js
+++ src/StoryReel.js
@@ -11,29 +11,29 @@ function StoryReel() {
             title='Vitor Hugo'
          />
          <Story 
-            image='https://scontent.fplu1-1.fna.fbcdn.net/v/t1.0-9/32239750_1693203777461242_78893575655391232_n.jpg?_nc_cat=101&_nc_sid=174925&_nc_eui2=AeFZR827-V0vrSwaLc3MMdKr26WJRkpqM4vbpYlGSmozi2ag5XumrK0hqJhgfGBu8cx-Ww0oQyNQWFYDIsmYakuT&_nc_ohc=B5k80MT-mTQAX94Okhy&_nc_ht=scontent.fplu1-1.fna&oh=7a1247ad82599df7574e4a6410926b53&oe=5FA8CBF8'
-            profileSrc='https://scontent.fplu1-1.fna.fbcdn.net/v/t1.0-9/72348943_2495246513923627_3964913099550490624_o.jpg?_nc_cat=109&_nc_sid=09cbfe&_nc_eui2=AeGXktDZ0pmbltdQC4P2v1EDhnKHDM0kh2GGcocMzSSHYbVl7IgO5ZQR3St7S4g-SMe-57Mv1BXpOdW8z4e7e2Qx&_nc_ohc=EkbtqBDc5t8AX97L92w&_nc_ht=scontent.fplu1-1.fna&oh=321ecaf6d88592f480e3b0c3c45949bf&oe=5FACB0CA'
-            title='Lucas Fernandes'
-         />
-         <Story 
             image='https://instagram.fplu1-1.fna.fbcdn.net/v/t51.2885-15/e35/p1080x1080/98173443_534822733854340_1431690426880947836_n.jpg?_nc_ht=instagram.fplu1-1.fna.fbcdn.net&_nc_cat=104&_nc_ohc=OO1ceZRKC0oAX-VXR95&_nc_tp=19&oh=896fa58f63932beef407c64715aa3122&oe=5FAF55AE'
             profileSrc='https://instagram.fplu1-1.fna.fbcdn.net/v/t51.2885-15/e35/53586726_133299421134722_3127766790969020502_n.jpg?_nc_ht=instagram.fplu1-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=I1IXLV6rKk4AX_FE6IR&_nc_tp=18&oh=03f5125ffe2da17ccb71c459045dfcc3&oe=5FAE94A9'
             title='Nicholas Ciacci'
          />
+            <Story 
+               image='https://instagram.fplu1-1.fna.fbcdn.net/v/t51.2885-15/e35/120260931_710563973138083_8727886083632069557_n.jpg?_nc_ht=instagram.fplu1-1.fna.fbcdn.net&_nc_cat=107&_nc_ohc=5bSehDHRU2YAX98XokO&_nc_tp=18&oh=388bd40f83ca9246e437b809288af23a&oe=5FAF927B'
+               profileSrc='https://instagram.fplu1-1.fna.fbcdn.net/v/t51.2885-15/e35/119725776_1161290380937451_8013486710924443636_n.jpg?_nc_ht=instagram.fplu1-1.fna.fbcdn.net&_nc_cat=103&_nc_ohc=pkBFJCy_vYcAX8JVZSv&_nc_tp=18&oh=460f56bfba43bdf0bc9b5f10be24ee20&oe=5FB0AB49'
+               title='Anna Clara'
+            />
+         <Story 
+            image='https://scontent.fplu1-1.fna.fbcdn.net/v/t1.0-9/32239750_1693203777461242_78893575655391232_n.jpg?_nc_cat=101&_nc_sid=174925&_nc_eui2=AeFZR827-V0vrSwaLc3MMdKr26WJRkpqM4vbpYlGSmozi2ag5XumrK0hqJhgfGBu8cx-Ww0oQyNQWFYDIsmYakuT&_nc_ohc=B5k80MT-mTQAX94Okhy&_nc_ht=scontent.fplu1-1.fna&oh=7a1247ad82599df7574e4a6410926b53&oe=5FA8CBF8'
+            profileSrc='https://scontent.fplu1-1.fna.fbcdn.net/v/t1.0-9/72348943_2495246513923627_3964913099550490624_o.jpg?_nc_cat=109&_nc_sid=09cbfe&_nc_eui2=AeGXktDZ0pmbltdQC4P2v1EDhnKHDM0kh2GGcocMzSSHYbVl7IgO5ZQR3St7S4g-SMe-57Mv1BXpOdW8z4e7e2Qx&_nc_ohc=EkbtqBDc5t8AX97L92w&_nc_ht=scontent.fplu1-1.fna&oh=321ecaf6d88592f480e3b0c3c45949bf&oe=5FACB0CA'
+            title='Lucas Fernandes'
+         />
+            <Story 
+               image='https://instagram.fplu1-1.fna.fbcdn.net/v/t51.2885-15/e35/119612942_373077783696033_9016731221706412526_n.jpg?_nc_ht=instagram.fplu1-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=vlqP32HhG-YAX9yB5bG&_nc_tp=18&oh=464332ea7e7cbf754e1f9bd050f3a43b&oe=5FB06B82'
+               profileSrc='https://instagram.fplu1-1.fna.fbcdn.net/v/t51.2885-15/e35/46541532_220266968897458_2862271069163503002_n.jpg?_nc_ht=instagram.fplu1-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=uIppKrTxHRkAX-kjHu6&_nc_tp=18&oh=c36e19454eb86717a70c6aaa01c51c7b&oe=5FAE4FB6'
+               title='Ana Lidia'
+            />
          <Story 
             image='https://instagram.fplu1-1.fna.fbcdn.net/v/t51.2885-15/e35/s1080x1080/80392153_1494804377350772_6811494805937439416_n.jpg?_nc_ht=instagram.fplu1-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=HEAY5ZE1rjcAX_Xk6GH&_nc_tp=15&oh=255f83d44aa635322daebcec42743cb0&oe=5FAD63DE'
             profileSrc='https://instagram.fplu1-1.fna.fbcdn.net/v/t51.2885-15/e35/p1080x1080/120247087_167583591686291_486804280504274788_n.jpg?_nc_ht=instagram.fplu1-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=wxBK5vGK4ikAX8i2wye&_nc_tp=19&oh=c1bc1fe59d9115c39ec8ab9cb65958f8&oe=5FAF1C15'
             title='Henrique Schraiber'
-         />
-         <Story 
-            image='https://cdn.pixabay.com/photo/2018/02/16/05/35/squirrel-3156987_960_720.jpg'
-            profileSrc='https://cdn.pixabay.com/photo/2013/10/09/02/26/beef-192976_960_720.jpg'
-            title='Lucas'
-         />
-         <Story 
-            image='https://cdn.pixabay.com/photo/2018/02/16/05/35/squirrel-3156987_960_720.jpg'
-            profileSrc='https://cdn.pixabay.com/photo/2013/10/09/02/26/beef-192976_960_720.jpg'
-            title='Lucas'
          />
       </div>
    )
